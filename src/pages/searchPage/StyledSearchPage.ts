79113c982f9bbd1ee6c4fa2df17faf097314ef77
oscargregorius <oscar.gregorius@gmail.com>
2021-09-20 15:53:30 +0200

    Made the Search page responsive

--- src/pages/searchPage/StyledSearchPage.ts
+++ src/pages/searchPage/StyledSearchPage.ts
@@ -1,13 +1,23 @@
 import styled from 'styled-components';
 
 export const StyledWrapper = styled.div`
-   padding: 0.5rem;   
+   padding: 0.5rem;
+   @media (min-width: 769px) {
+    width: 80%;
+    margin: 0 auto;
+  }
 `
 
 export const StyledSongWrapper = styled.div`
   display: grid;
-  grid-template-columns: 20% 1fr 10%;
+  grid-template-columns: 4.5rem 1fr 2rem;
   border-bottom: 1px solid #9C27B0;
+  @media (min-width: 769px) {
+   &:hover{
+     background:#F5F5F5;
+     cursor: pointer;
+   }
+  }
 `
 
 export const StyledSongs = styled.p`
