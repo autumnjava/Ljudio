6f115c01fe2426a66161702c3d6ff56b224e9ba1
autumnjava <creizy@gmail.com>
2021-09-21 15:38:09 +0200

    set up server so that we can send requests from other localhost

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 // import mongoose module
 const mongoose = require('mongoose');
 const express = require('express');
-const app = express();
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const gql = require('graphql-tag');
 
 const graphQlResolvers = require('./graphql/resolvers/index');
 const schema = require('./GraphQL/schema/schema');
+
+const app = express();
+
+// we need this to allow requests from another server (in this case localhost:3000)
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if(req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+})
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
