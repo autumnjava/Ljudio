41b529594ab904e75a56a18f3aaad088bb063f90
oscargregorius <oscar.gregorius@gmail.com>
2021-09-21 14:48:42 +0200

    Click events for the play/paus
    Made click event for play/paus.
    If u click on play the icon will switch to paus

--- src/components/miniPlayer/MiniPlayer.tsx
+++ src/components/miniPlayer/MiniPlayer.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { PlaylistContext } from '../../contexts/playlistsContext/PlaylistContextProvider';
 import {
   StyledWrapper,
@@ -7,13 +7,57 @@ import {
 } from './StyledMiniPlayer'
 import BottomNavigation from '@mui/material/BottomNavigation';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 
 
 const MiniPlayer = () => {
 
+  const [play, setPlay] = useState(true);
   const songs = useContext(PlaylistContext);
+
+  const handlePlay = () => {
+    setPlay(!play)
+  }
+
+  const handlePaus = () => {
+    setPlay(!play)
+  }
+
+
+  const renderIcons = () => (
+    <StyledPlayerWrapper>
+      <SkipPreviousIcon style={{
+        alignSelf: 'center',
+        justifySelf: 'center',
+        fontSize: '2.5rem',
+        color: 'white'
+      }} />
+     {play ? <PlayArrowIcon style={{
+        alignSelf: 'center',
+          justifySelf: 'center',
+        fontSize: '3.5rem',
+        color: 'white'
+      }}
+      onClick={handlePlay}/>
+      :
+      <PauseIcon style={{
+        alignSelf: 'center',
+          justifySelf: 'center',
+        fontSize: '3.5rem',
+        color: 'white'
+        }}
+        onClick={handlePaus}/>}
+      <SkipNextIcon style={{
+        alignSelf: 'center',
+        justifySelf: 'center',
+        fontSize: '2.5rem',
+        color: 'white'
+      }} />
+    </StyledPlayerWrapper>
+  )
+
 
   return (
     <>
@@ -24,26 +68,7 @@ const MiniPlayer = () => {
           display: 'grid'
         }} sx={{ width: '100vw', height: '5rem' }}>
           {songs?.currentSong && <StyledSongTitle>{songs.currentSong.snippet.title}</StyledSongTitle>}
-          <StyledPlayerWrapper>
-              <SkipPreviousIcon style={{
-                alignSelf: 'center',
-                justifySelf: 'center',
-                fontSize: '2.5rem',
-                color: 'white'
-              }} />
-              <PlayArrowIcon style={{
-                alignSelf: 'center',
-                 justifySelf: 'center',
-                fontSize: '3.5rem',
-                color: 'white'
-              }} />
-              <SkipNextIcon style={{
-                alignSelf: 'center',
-                justifySelf: 'center',
-                fontSize: '2.5rem',
-                color: 'white'
-              }} />
-            </StyledPlayerWrapper>
+          {renderIcons()}
         </BottomNavigation>
       </StyledWrapper>
     </>
