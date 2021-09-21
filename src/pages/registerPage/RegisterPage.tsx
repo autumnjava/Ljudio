d285ae389b4bbcb8be570858a3cacd1298007944
Ali94160 <ali.al-mousawi@hotmail.com>
2021-09-21 14:57:24 +0200

    Created the Register Form.

--- src/pages/registerPage/RegisterPage.tsx
+++ src/pages/registerPage/RegisterPage.tsx
@@ -1,11 +1,47 @@
-import { StyledTitle } from "./StyledRegisterPage";
+import {
+  StyledTitle,
+  StyledWrapper,
+  StyledForm,
+  StyledInput,
+  StyledInputWrapper,
+  StyledLabel,
+  StyledButton
+} from "./StyledRegisterPage";
 
 
 const RegisterPage: React.FC = () => {
+
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+  }
+
+
   return (
-    <>
+    <StyledWrapper>
+      <StyledForm onSubmit={ (e) => submitHandler(e)}>
+    
         <StyledTitle>REGISTER</StyledTitle>
-    </>
+        
+        <StyledInputWrapper>
+            <StyledLabel>Username</StyledLabel>
+          <StyledInput />
+          
+          <StyledLabel>Email</StyledLabel>
+          <StyledInput />
+          
+          <StyledLabel>Password</StyledLabel>
+          <StyledInput />
+          
+          <StyledLabel>Password</StyledLabel>
+            <StyledInput/>
+        </StyledInputWrapper>
+
+        <StyledButton>CREATE ACCOUNT</StyledButton>
+        
+      </StyledForm>
+      </StyledWrapper>
   )
 }
 
--- src/pages/registerPage/StyledRegisterPage.ts
+++ src/pages/registerPage/StyledRegisterPage.ts
@@ -4,3 +4,51 @@ export const StyledTitle = styled.h2`
   font-family: Arial, Helvetica, sans-serif;
   letter-spacing: 0.2rem;
 `
+
+export const StyledWrapper = styled.div`
+`;
+
+
+export const StyledForm = styled.form`
+  text-align: center;
+  margin: 5rem auto;
+`;
+
+export const StyledInput = styled.input`
+  text-decoration: none;
+  outline: none;
+  font-size: 20px;
+  border: none;
+  border-bottom: 1px solid pink;
+   &:focus {
+        color: green;
+        border-bottom: 1px solid #EF02EB;
+    }
+
+`;
+
+export const StyledLabel = styled.p`
+  color: #EF02EB;
+  opacity: 80%;
+
+`;
+
+export const StyledInputWrapper = styled.div`
+  display: block;
+`;
+
+export const StyledButton = styled.button`
+  text-decoration: none;
+  outline: none;
+  opacity: 80%;
+  margin: 1rem;
+  border: none;
+  background-color: pink;
+  border-radius: 0.2rem;
+  font-size: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+  letter-spacing: 0.15rem;
+  &:hover{
+    opacity: 100%;
+  }
+`;
