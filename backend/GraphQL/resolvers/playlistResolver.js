2f66982dc68bf8119ae677ae9bab6104fe65c03c
postila <isabella.postila@hotmail.com>
2021-09-22 17:19:36 +0200

    create & remove resolvers
    a playlist is created and added to a users list, and a playlist can be deleted from user and from the db
    
    Co-Authored-By: bextex <72066237+bextex@users.noreply.github.com>

--- backend/GraphQL/resolvers/playlistResolver.js
+++ backend/GraphQL/resolvers/playlistResolver.js
@@ -1,19 +1,56 @@
 const Playlist = require('../../models/playlist');
+const User = require('../../models/user');
 
 const playlistResolver = {
   createPlaylist: async (args) => {
     
-    let userPlaylists = [];
-
-    const playlist = await new Playlist({
+    const playlist = new Playlist({
       name: args.name,
-      creator: args.creator
     })
-      
-    const newPlaylist = playlist.save();
-    userPlaylists.push(newPlaylist);
-    return userPlaylists
-  }  
+
+    const savedPlaylist = await playlist.save();
+
+    await User.findOneAndUpdate({
+      _id: args.userId
+    },
+      {
+        $push: {
+          myPlaylists: {
+            _id: savedPlaylist._id
+          }
+        }
+      })
+
+    return await playlist;
+  },
+
+  removePlaylist: async (args) => {
+
+    const playlist = await Playlist.findOneAndDelete({ _id: args._id });
+    
+        await User.updateOne({
+          _id: args.userId
+        }, {
+          $pull: {
+            myPlaylists: playlist._id
+          }
+        })
+
+    // await User.aggregate([{ $unwind: "$myPlaylists" }]);
+
+    // const users = await User.findOne({
+    //   _id: args.userId
+    // },
+    //   {
+    //     myPlaylists: {
+    //       $elemMatch: {
+    //         _id: args._id
+    //       }
+    //     }
+    //   })
+
+    return playlist
+  }
 };
 
 module.exports = playlistResolver;
