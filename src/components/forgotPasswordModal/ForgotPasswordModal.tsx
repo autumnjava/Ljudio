be06435d675de6d60480389a85d696942f4f9028
autumnjava <creizy@gmail.com>
2021-09-24 13:23:03 +0200

    design fix

--- src/components/forgotPasswordModal/ForgotPasswordModal.tsx
+++ src/components/forgotPasswordModal/ForgotPasswordModal.tsx
@@ -1,6 +1,17 @@
+import React, {  useState } from "react";
+
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+
+import {
+  StyledWrapper,
+  StyledForm,
+  StyledInput,
+  StyledInputWrapper,
+  StyledLabel,
+  StyledButton,
+} from "./StyledForgotPassword";
 
 interface ModalProps {
   open: boolean,
@@ -12,6 +23,9 @@ const ForgotPasswordModal = (props: ModalProps): JSX.Element => {
     open,
     handleClose
   } = props;
+
+  const [email, setEmail] = useState('');
+
 
   const style = {
     position: 'absolute' as const,
@@ -33,12 +47,29 @@ const ForgotPasswordModal = (props: ModalProps): JSX.Element => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <Typography align='center' id="modal-modal-title" variant="h6" component="h2">
             Forgot your password?
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <Typography align='center' id="modal-modal-description" sx={{ mt: 2 }}>
             Don&apos;t worry, happens to the best of us.
           </Typography>
+
+          <StyledWrapper>
+      <StyledForm>
+
+        <StyledInputWrapper>
+          <StyledLabel>Email</StyledLabel>
+          <StyledInput value={email}
+                onChange={(e) => setEmail(e.target.value)} />
+        </StyledInputWrapper>
+
+        <StyledButton onClick={() => {
+          handleClose();
+          setEmail('')
+          }
+          }> Send new password</StyledButton>
+      </StyledForm>
+      </StyledWrapper>
         </Box>
       </Modal>
   )
