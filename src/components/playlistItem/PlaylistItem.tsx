0415411a980e7765ec30a86525fa19def0261fb1
Ali94160 <ali.al-mousawi@hotmail.com>
2021-09-24 08:57:41 +0200

    Created route for specific playlist by ID in URL.

--- src/components/playlistItem/PlaylistItem.tsx
+++ src/components/playlistItem/PlaylistItem.tsx
@@ -1,16 +1,19 @@
 import {StyledItemDiv, StyledImg, StyledPlaylistWrapper, StyledListTitle } from "./StyledPlaylistItem";
-
+import { useHistory } from "react-router-dom";
 
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const PlaylistItem = ({data}: any) => {
+const PlaylistItem = ({ data }: any) => {
+
+  const history = useHistory();
+  
   return (
     <>
       <StyledPlaylistWrapper>
 
         
 
-      <StyledItemDiv>
+      <StyledItemDiv onClick={() => history.push("/playlist/" + data._id)}>
         <div>
           <StyledImg src="https://i.postimg.cc/nVmnQDCz/analyze-sound-wave-music-512-362.png" alt="" />
         </div>
--- src/pages/playlistPage/PlaylistPage.tsx
+++ src/pages/playlistPage/PlaylistPage.tsx
@@ -1,10 +1,15 @@
 import { StyledTitle } from "./StyledPlaylistPage";
-
+import { useParams } from "react-router";
 
 const PlaylistPage = () => {
+
+  const { id }: any = useParams();
+  
   return (
     <>
       <StyledTitle>Playlist</StyledTitle>
+
+
     </>
   )
 }
