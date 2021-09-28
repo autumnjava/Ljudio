c61854a31c9ed8ddf758f1da98e490ab99c058db
Ali94160 <ali.al-mousawi@hotmail.com>
2021-09-28 14:25:22 +0200

    created Dj room page.

---
+++ src/pages/djRoomPage/DjRoomPage.tsx
@@ -0,0 +1,12 @@
+import { StyledWrapper } from "./StyledDjRoomPage";
+
+
+const DjRoomPage = () => {
+  return (
+    <StyledWrapper>
+      <p>ALL ONLINE DJ ROOMS</p>
+    </StyledWrapper>
+  );
+}
+
+export default DjRoomPage;
