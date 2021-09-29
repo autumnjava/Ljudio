ff180e7e076679238ccfe82937fc983417c27d29
oscargregorius <oscar.gregorius@gmail.com>
2021-09-29 14:37:01 +0200

    Added a play all btn with styling

--- src/pages/playlistPage/PlaylistPage.tsx
+++ src/pages/playlistPage/PlaylistPage.tsx
@@ -1,7 +1,8 @@
-import { StyledPLTitle } from "./StyledPlaylistPage";
+import { StyledPLTitle,StyledHeadWrapper,StyledButton } from "./StyledPlaylistPage";
 import PlaylistRowItem from "../../components/playlistRowItem/PlaylistRowItem";
 import { useContext, useEffect, useState } from 'react';
 import { PlaylistContext } from '../../contexts/playlistsContext/PlaylistContextProvider';
+import Button from '@mui/material/Button';
 import { useParams } from "react-router-dom";
 
 interface SongProps {
@@ -38,10 +39,10 @@ const PlaylistPage = () => {
     <>
       {console.log('here',playlist.songs)}
       <div>
-        {playlist ? <div>
+        {playlist ? <StyledHeadWrapper>
           <StyledPLTitle>{playlist.name}</StyledPLTitle>
-          
-        </div> : <p style={{marginTop: "55px"}}>NAME NOT FOUND...</p>}
+          <StyledButton>PLAY ALL</StyledButton>
+        </StyledHeadWrapper> : <p style={{marginTop: "55px"}}>NAME NOT FOUND...</p>}
         {playlist.songs && playlist.songs.map((song: SongProps) => {
           return <PlaylistRowItem key={song._id} song={song} playlistId={playlist._id} />
         })}
