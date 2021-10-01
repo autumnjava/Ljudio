804f0c788d8fbcfe44ef4c55a4bbb9220c55c337
postila <isabella.postila@hotmail.com>
2021-10-01 10:36:07 +0200

    created mutation query for createDjRoom

--- src/contexts/djRoomContext/djRoomContextProvider.tsx
+++ src/contexts/djRoomContext/djRoomContextProvider.tsx
@@ -1,14 +1,50 @@
 import { createContext, useState } from 'react';
+import fetcher from '../fetcher';
 
 type Props = {
   children?: JSX.Element
 }
 
+interface djRoomProps{
+  name: string,
+  description: string,
+  imgUrl: string,
+  isOnline: boolean
+}
+
 export const DjRoomContext = createContext<any>(null);
 
 export const DjRoomProvider: React.FC<Props> = ({ children }: Props) => {
+  const [errorMsg, setErrorMsg] = useState(false);
+
+
+  const createDjRoom = async (userId: string, playlistId: string, input: djRoomProps) => {
+    const requestBody = {
+      query: `mutation{createDjRoom(
+        userId: "${userId}",
+        playlistId:"${playlistId}"
+      input:{
+        name:"${input.name}",
+        description:"${input.description}",
+        image:"${input.imgUrl}",
+        isOnline: ${input.isOnline}})
+      {
+        _id
+        name
+      }
+    }`
+    }
+    const response = await fetcher(requestBody);
+    if (!response) {
+      setErrorMsg(true);
+    } else {
+      console.log(response.data)
+    }
+  }
+
   
-   const values = {
+  const values = {
+     createDjRoom
   }
 
   return (
