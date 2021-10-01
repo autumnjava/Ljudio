e32a0299b696d9ee53253cf27a6b286662d604f9
postila <isabella.postila@hotmail.com>
2021-10-01 11:24:58 +0200

    created joinDjRoom request

--- src/contexts/djRoomContext/djRoomContextProvider.tsx
+++ src/contexts/djRoomContext/djRoomContextProvider.tsx
@@ -21,30 +21,65 @@ export const DjRoomProvider: React.FC<Props> = ({ children }: Props) => {
   const createDjRoom = async (userId: string, playlistId: string, input: djRoomProps) => {
     const requestBody = {
       query: `mutation{createDjRoom(
-        userId: "${userId}",
-        playlistId:"${playlistId}"
-      input:{
-        name:"${input.name}",
-        description:"${input.description}",
-        image:"${input.imgUrl}",
-        isOnline: ${input.isOnline}})
-      {
-        _id
-        name
-      }
-    }`
+          userId: "${userId}",
+          playlistId:"${playlistId}"
+        input:{
+          name:"${input.name}",
+          description:"${input.description}",
+          image:"${input.imgUrl}",
+          isOnline: ${input.isOnline}})
+        {
+          _id
+          name
+        }
+      }`
     }
     const response = await fetcher(requestBody);
     if (!response) {
       setErrorMsg(true);
     } else {
       console.log(response.data)
+      setErrorMsg(false);
     }
   }
 
+  const deleteDjRoom = async () => {
+    const requestBody = {
+      query: ``
+    }
+    const response = await fetcher(requestBody);
+    if (!response) {
+      setErrorMsg(true);
+    } else {
+      console.log(response.data)
+      setErrorMsg(false);
+    }
+  }
+
+  const joinDjRoom = async (userId: string, djRoomId: string) => {
+    const requestBody = {
+      query: `mutation{joinDjRoom(
+        _id:"${userId}",
+        djRoomId:"${djRoomId}"
+        ){
+          _id
+          name
+        }
+      }`
+    }
+    const response = await fetcher(requestBody);
+    if (!response) {
+      setErrorMsg(true);
+    } else {
+      console.log(response.data)
+      setErrorMsg(false);
+    }
+  }
   
   const values = {
-     createDjRoom
+    createDjRoom,
+    deleteDjRoom,
+    joinDjRoom,
   }
 
   return (
