3c330440b170d64f3e1223d2d0576a96b68f8431
Ali94160 <ali.al-mousawi@hotmail.com>
2021-10-05 10:46:57 +0200

    display myplaylist on homePage

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,27 +1,68 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import {UserContext} from '../../contexts/usersContext/UserContextProvider'
-import { StyledTitle } from "./StyledHomePage"
 import { useHistory } from "react-router";
 import SearchField from '../../components/searchField/SearchField';
+import logo from "../landingPage/videos/logo.png"
 import { PlaylistContext } from "../../contexts/playlistsContext/PlaylistContextProvider";
+import {
+  StyledLogoImg,
+  StyledWrapper,
+  StyledImgDiv
+} from "./StyledHomePage"
+import PlaylistItem from "../../components/playlistItem/PlaylistItem";
+
+
+interface List {
+  name: string;
+  _id: string;
+}
+
+
 
 const HomePage: React.FC = () => {
   const { logout } = useContext(UserContext);
   const { handleSearch } = useContext(PlaylistContext);
   const history = useHistory();
+  const { playlists, getUserPlaylists, deletePlaylist, createPlaylist } = useContext(PlaylistContext);
+  const [userId, setUserId] = useState<string | null>('');
 
   const logoutHandler = async () => {
     logout();
     history.push('/');
     window.location.reload();
   }
+
+
+    useEffect(() => {
+    const userId = localStorage.getItem('userId');
+    setUserId(userId);
+  }, []);
+
+  useEffect(() => {
+    if (userId) {
+      myPlaylists();
+
+    }
+  }, [!playlists, userId]);
+
+
+  const myPlaylists = async () => {
+    await getUserPlaylists(userId);
+  }
   
   return (
-    <>
-      <StyledTitle>HOME</StyledTitle>
+    <StyledWrapper>
+      <StyledImgDiv style={{margin: '0 auto'}}>
+      <StyledLogoImg src={logo}/>
+      </StyledImgDiv>
+
         <SearchField handleArtistSearch={handleSearch} handleYoutubeSearch={handleSearch} />
   
-    </>
+      <h2>Your Playlists:</h2>
+      {playlists && playlists.map((list: List) => {
+        return <PlaylistItem key={list._id} data={[list]}  />
+        })}
+    </StyledWrapper>
   )
 }
 
--- src/pages/homePage/StyledHomePage.ts
+++ src/pages/homePage/StyledHomePage.ts
@@ -1,6 +1,21 @@
 import styled from "styled-components";
 
-export const StyledTitle = styled.h2`
-  font-family: Arial, Helvetica, sans-serif;
-  letter-spacing: 0.2rem;
-`
+export const StyledLogoImg = styled.img`
+  width: 8rem;
+  align-items: center;
+  display: flex;
+  margin: 0 auto;
+  position: relative;
+  margin-top: 2rem;
+  padding-bottom: 0.2rem;
+    @media (min-width: 769px) {
+      margin-top: 6rem;
+   }
+`;
+
+export const StyledWrapper = styled.div`
+
+`;
+export const StyledImgDiv = styled.div`
+  margin: 0 auto;
+`;
