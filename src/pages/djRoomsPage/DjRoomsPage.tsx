64735912c417e831c49aa075ab024a0a81f3c3d6
postila <isabella.postila@hotmail.com>
2021-10-05 10:39:00 +0200

    user can join djRoom from page of all active djRooms

--- src/pages/djRoomsPage/DjRoomsPage.tsx
+++ src/pages/djRoomsPage/DjRoomsPage.tsx
@@ -5,23 +5,34 @@ import {
 } from "./StyledDjRoomPage";
 import banner from "./images/banner.png"
 import { DjRoomContext } from '../../contexts/djRoomContext/djRoomContextProvider';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 const DjRoomsPage = () => {
-  const { activeDjRooms, getActiveDjRooms } = useContext(DjRoomContext);
+  const { activeDjRooms, getActiveDjRooms, joinDjRoom } = useContext(DjRoomContext);
+  const [userId, setUserId] = useState<string | null>('');
+
 
   useEffect(() => {
     getAllDjRooms()
+  }, []);
+
+  useEffect(() => {
+    const userId = localStorage.getItem('userId');
+    setUserId(userId);
   }, []);
 
   const getAllDjRooms = async () => {
     await getActiveDjRooms();
   }
 
+  const handleJoinDjRoom = async (djRoomId: string) => {
+    await joinDjRoom(userId, djRoomId);
+  }
+
   return (
     <StyledWrapper>
       <StyledPageTitle>JOIN THE DJ ROOMS AND LISSTEN MUSIC TOGETHER!</StyledPageTitle>
-      {activeDjRooms && activeDjRooms.map((room: any) => <DjRoomRowItem key={room._id} data={room} />)}
+      {activeDjRooms && activeDjRooms.map((room: any) => <DjRoomRowItem key={room._id} data={[room, handleJoinDjRoom]} />)}
     </StyledWrapper>
   );
 }
